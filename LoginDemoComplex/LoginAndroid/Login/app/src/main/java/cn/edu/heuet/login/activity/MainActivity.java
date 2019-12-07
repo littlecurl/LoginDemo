9c74b17d6935b248c7dd2ebac4c8c7457887fd0d
LittleCurl <littlecurl@qq.com>
2019-12-07 21:07:43 +0800

    更新依赖为androidx、重新布局登录页

--- LoginDemoComplex/LoginAndroid/Login/app/src/main/java/cn/edu/heuet/login/activity/MainActivity.java
+++ LoginDemoComplex/LoginAndroid/Login/app/src/main/java/cn/edu/heuet/login/activity/MainActivity.java
@@ -1,6 +1,6 @@
 package cn.edu.heuet.login.activity;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import cn.edu.heuet.login.R;
