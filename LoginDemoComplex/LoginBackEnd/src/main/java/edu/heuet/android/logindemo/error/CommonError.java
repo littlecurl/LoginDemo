8a440acd4093324a4ec082c0f9872b14ab491f0c
littlecurl <424171723@qq.com>
2021-01-13 15:16:58 +0800

    修复App.java中扫描包路径错误的问题； 兼容Mysql 6.0+版本

--- LoginDemoComplex/LoginBackEnd/src/main/java/edu/heuet/android/logindemo/error/CommonError.java
+++
@@ -1,7 +0,0 @@
-package edu.heuet.android.logindemo.error;
-
-public interface CommonError {
-    public int getCode();
-    public String getMsg();
-    public CommonError setMsg(String errorMsg);
-}
